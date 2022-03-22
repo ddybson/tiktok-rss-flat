e23a6dfdd6e91eaab22b88bf0376dd97709ef976
ddybson <61180332+ddybson@users.noreply.github.com>
2022-03-22 12:21:24 +0100

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -23,10 +23,10 @@ with open('subscriptions.csv') as f:
         fg = FeedGenerator()
         fg.id('https://www.tiktok.com/@' + user)
         fg.title(user + ' TikTok')
-        fg.author( {'name':'Conor ONeill','email':'conor@conoroneill.com'} )
+        fg.author( {'name':'RMF','email':'daniel.dybek@rmf.pl'} )
         fg.link( href='http://tiktok.com', rel='alternate' )
         fg.logo(ghPagesURL + 'tiktok-rss.png')
-        fg.subtitle('OK Boomer, all the latest TikToks from ' + user)
+        fg.subtitle('Latest TikToks from ' + user)
         fg.link( href=ghPagesURL + 'rss/' + user + '.xml', rel='self' )
         fg.language('en')
 
